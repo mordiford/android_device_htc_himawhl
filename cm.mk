67ee71dbb69b670f5957753e8949ab1e2256584b
Rashed Abdel-Tawab <rashed@linux.com>
2015-07-25 17:12:05 -0400

    himawl: Initial commit

---
+++ cm.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/htc/himawl/full_himawl.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+PRODUCT_NAME := cm_himawl
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="htc_himaulatt" \
+    PRODUCT_NAME="himaulatt_na_gen_unlock" \
+    BUILD_FINGERPRINT="htc/HTCOneM9vzw/htc_himawl:5.0.2/LRX22G/511781.15:user/release-keys" \
+    PRIVATE_BUILD_DESC="1.33.605.15 CL511781 release-keys"
