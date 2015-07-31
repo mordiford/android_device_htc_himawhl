af0dd341e0026e25e85a341f7f11fce2bf5a6e86
Paul Keith <javelinanddart@gmail.com>
2015-07-31 13:46:37 -0500

    himawl: Update Tree
     * Minor renaming to himawlvzw from himaulatt
     * Redo RIL system.prop: Copy from himaul, patch from jfltevzw
    
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 PRODUCT_NAME := cm_himawl
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="htc_himaulatt" \
-    PRODUCT_NAME="himaulatt_na_gen_unlock" \
+    PRODUCT_DEVICE="htc_himawlvzw" \
+    PRODUCT_NAME="himawlvzw_na_gen_unlock" \
     BUILD_FINGERPRINT="htc/HTCOneM9vzw/htc_himawl:5.0.2/LRX22G/511781.15:user/release-keys" \
     PRIVATE_BUILD_DESC="1.33.605.15 CL511781 release-keys"
