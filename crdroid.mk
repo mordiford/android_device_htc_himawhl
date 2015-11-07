0a9fadf93bf302f5a057ebaf4e50bccb415c8c09
irocevo <irocevo@users.noreply.github.com>
2015-11-07 13:02:23 -0500

    Update and rename cm.mk to crdroid.mk

--- crdroid.mk
+++ crdroid.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/htc/himawhl/full_himawhl.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common crdroid stuff.
+$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/crdroid/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_himawhl
+PRODUCT_NAME := crdroid_himawhl
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himawhlspr" \
