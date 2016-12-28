58de2af80432a06f218e8e1bc67c36aab4c0c4fd
Mirenk <minikmwinsea@gmail.com>
2016-12-28 18:40:34 +0900

    himawhl: Update fingerprint from 3.41.651.41

--- aicp.mk
+++ aicp.mk
@@ -12,10 +12,10 @@ $(call inherit-product, vendor/aicp/configs/bootanimation.mk)
 PRODUCT_NAME := aicp_himawhl
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE="htc_himawhlspr" \
-    PRODUCT_NAME="himawhlspr_na_gen_unlock" \
-    BUILD_FINGERPRINT="htc/HTCOneM9spr/htc_himawhl:5.0.2/LRX22G/511781.15:user/release-keys" \
-    PRIVATE_BUILD_DESC="1.33.605.15 CL511781 release-keys"
+    PRODUCT_DEVICE="htc_himawhl" \
+    PRODUCT_NAME="himawhl_sprint_wwe" \
+    BUILD_FINGERPRINT=htc/himawhl_sprint_wwe/htc_himawhl:6.0/MRA58K/695981.41:user/release-keys \
+    PRIVATE_BUILD_DESC="3.41.651.41 CL695981 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
