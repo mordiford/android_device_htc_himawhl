8276087c4ea789d22392db205d5dd196be7113de
Mirenk <minikmwinsea@gmail.com>
2017-10-14 07:01:29 +0900

    himawhl: Update fingerprint

--- aicp.mk
+++ aicp.mk
@@ -14,8 +14,8 @@ PRODUCT_NAME := aicp_himawhl
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himawhl" \
     PRODUCT_NAME="himawhl_sprint_wwe" \
-    BUILD_FINGERPRINT=htc/himawhl_sprint_wwe/htc_himawhl:6.0/MRA58K/695981.41:user/release-keys \
-    PRIVATE_BUILD_DESC="3.41.651.41 CL695981 release-keys"
+    BUILD_FINGERPRINT="htc/himawhl_sprint_wwe/htc_himawhl:7.0/NRD90M/870580.4:user/release-keys" \
+    PRIVATE_BUILD_DESC="4.27.651.4 CL870580 release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
