bd86c8fb8bb0e84b7fb567b73e33110e4dd1459f
razorloves <razorloves@gmail.com>
2018-04-02 18:03:32 -0500

    himawl: set build fingerprint instead of override
    Change-Id: Iee35bc558d44baf45d0c735ac44e64a1060f4f03

--- lineage.mk
+++ lineage.mk
@@ -10,5 +10,6 @@ PRODUCT_NAME := lineage_himawl
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himawl" \
     PRODUCT_NAME="HTCOneM9vzw" \
-    BUILD_FINGERPRINT="htc/HTCOneM9vzw/htc_himawl:7.0/NRD90M/947620.11:user/release-keys" \
     PRIVATE_BUILD_DESC="4.49.605.11 CL947620 release-keys"
+
+BUILD_FINGERPRINT := "htc/HTCOneM9vzw/htc_himawl:7.0/NRD90M/947620.11:user/release-keys"
